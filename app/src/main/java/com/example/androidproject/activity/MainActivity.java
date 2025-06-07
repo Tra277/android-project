a92123d54dc745c0b028985b5725c34a9e501690
Vu Nguyen <vunguyen2300@gmail.com>
2025-06-07 22:11:17 +0700

    update

--- app/src/main/java/com/example/androidproject/activity/MainActivity.java
+++ app/src/main/java/com/example/androidproject/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidproject.activity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,6 +10,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.androidproject.R;
+import com.example.androidproject.db.DatabaseHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        DatabaseHelper dbHelper = new DatabaseHelper(this);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
